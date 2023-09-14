5688a972f1673e62198546250b632a6619fed637
Christian Mestas Zegarra <cmestasz@unsa.edu.pe>
2023-09-13 21:31:30 -0500

    Actividad 3

--- fase01/lab01/Actividades.java
+++ fase01/lab01/Actividades.java
@@ -1,4 +1,4 @@
-// Laboratorio Nro 1 - Actividad 2
+// Laboratorio Nro 1 - Actividad 3
 // Autor: Christian Mestas
 
 import java.util.Scanner;
@@ -6,25 +6,12 @@ import java.util.Scanner;
 public class Actividades {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String nombreSoldado1 = sc.nextLine();
-        int vidaSoldado1 = sc.nextInt();
-        sc.nextLine();
-        String nombreSoldado2 = sc.nextLine();
-        int vidaSoldado2 = sc.nextInt();
-        sc.nextLine();
-        String nombreSoldado3 = sc.nextLine();
-        int vidaSoldado3 = sc.nextInt();
-        sc.nextLine();
-        String nombreSoldado4 = sc.nextLine();
-        int vidaSoldado4 = sc.nextInt();
-        sc.nextLine();
-        String nombreSoldado5 = sc.nextLine();
-        int vidaSoldado5 = sc.nextInt();
-        sc.nextLine();
-        System.out.println("Soldado 1: " + nombreSoldado1 + ". Vida: " + vidaSoldado1);
-        System.out.println("Soldado 2: " + nombreSoldado2 + ". Vida: " + vidaSoldado2);
-        System.out.println("Soldado 3: " + nombreSoldado3 + ". Vida: " + vidaSoldado3);
-        System.out.println("Soldado 4: " + nombreSoldado4 + ". Vida: " + vidaSoldado4);
-        System.out.println("Soldado 5: " + nombreSoldado5 + ". Vida: " + vidaSoldado5);
+        String[] nombresSoldados = new String[5];
+        for (int i = 0; i < nombresSoldados.length; i++) {
+            nombresSoldados[i] = sc.nextLine();
+        }
+        for (int i = 0; i < nombresSoldados.length; i++) {
+            System.out.println("Soldado " + (i + 1) + ": " + nombresSoldados[i]);
+        }
     }
 }
