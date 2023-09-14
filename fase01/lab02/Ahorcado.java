bb796c6d11d04aedba620456a4ea421de020188f
Christian Mestas Zegarra <cmestasz@unsa.edu.pe>
2023-09-13 21:50:40 -0500

    Folder para lab02 y archivo Ahorcado.java creado

---
+++ fase01/lab02/Ahorcado.java
@@ -0,0 +1,11 @@
+// Laboratorio Nro 1 - Actividad 1
+// Autor: Christian Mestas
+// Colaboró: Marco Aedo, version del ahorcado al 70%
+
+import java.util.Scanner;
+
+public class Ahorcado {
+    public static void main(String[] args) {
+        
+    }
+}
