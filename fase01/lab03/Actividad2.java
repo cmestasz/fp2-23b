7c3dacbd75ac557ab7d8ed4f5e6d1498c5fb00d0
Christian Mestas Zegarra <cmestasz@unsa.edu.pe>
2023-09-19 18:51:35 -0500

    Plantillas de archivos necesarios para resolver las actividades del laboratorio

---
+++ fase01/lab03/Actividad2.java
@@ -0,0 +1,8 @@
+// Laboratorio Nro 3 - Actividad 2
+// Autor: Christian Mestas
+
+public class Actividad2 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ fase01/lab03/Actividad3.java
@@ -0,0 +1,8 @@
+// Laboratorio Nro 3 - Actividad 3
+// Autor: Christian Mestas
+
+public class Actividad3 {
+    public static void main(String[] args) {
+        
+    }
+}
