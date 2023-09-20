2b453453176b2bd7d4092b58d2570f4430c098f1
Christian Mestas Zegarra <cmestasz@unsa.edu.pe>
2023-09-19 19:52:47 -0500

    Actividad 2

--- fase01/lab03/Actividad2.java
+++ fase01/lab03/Actividad2.java
@@ -1,8 +1,21 @@
 // Laboratorio Nro 3 - Actividad 2
 // Autor: Christian Mestas
 
+import java.util.Scanner;
+
 public class Actividad2 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        Soldado[] soldados = new Soldado[5];
+        for (int i = 0; i < soldados.length; i++) {
+            String nombre = sc.nextLine();
+            int vida = sc.nextInt();
+            sc.nextLine();
+            soldados[i] = new Soldado(nombre, vida);
+        }
+        for (int i = 0; i < soldados.length; i++) {
+            System.out.println("Soldado " + (i + 1) + ":");
+            System.out.println(soldados[i]);
+        }
     }
 }
