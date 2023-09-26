b254c3ae34591a489fa2377760bb00021c00be93
Christian Mestas Zegarra <cmestasz@unsa.edu.pe>
2023-09-25 19:12:05 -0500

    Plantillas de archivos necesarios para resolver las actividades del laboratorio

---
+++ fase01/lab05/Soldado.java
@@ -0,0 +1,29 @@
+public class Soldado {
+    private String nombre;
+    private int vida;
+
+    public Soldado(String nombre, int vida) {
+        setNombre(nombre);
+        setVida(vida);
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public String toString() {
+        return ("Nombre: " + nombre + ". Vida: " + vida);
+    }
+}
