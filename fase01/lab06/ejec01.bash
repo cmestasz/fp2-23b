2217626cfae815433a0bd22f9e91047617b3c001
Christian Mestas Zegarra <cmestasz@unsa.edu.pe>
2023-10-12 08:11:35 -0500

    Informe del laboratorio 6

---
+++ fase01/lab06/ejec01.bash
@@ -0,0 +1,87 @@
+           A     B     C     D     E     F     G     H     I     J   
+        -------------------------------------------------------------
+1       |     |     |     |     |     |     |     |     | 3X2 |     |
+        -------------------------------------------------------------
+2       | 8X1 |     |     |     |     |     |     |     |     |     |
+        -------------------------------------------------------------
+3       |     |     |     | 4X2 |     |     |     |     |     | 6X1 |
+        -------------------------------------------------------------
+4       |     |     |     |     |     |     |     |     |     |     |
+        -------------------------------------------------------------
+5       |     |     |     |     | 1X2 |     |     |     |     | 0X1 |
+        -------------------------------------------------------------
+6       |     | 2X2 |     |     |     | 6X2 |     |     |     |     |
+        -------------------------------------------------------------
+7       |     |     |     |     |     |     |     | 3X1 |     | 5X2 |
+        -------------------------------------------------------------
+8       |     |     | 4X1 |     |     |     | 8X2 | 1X1 |     |     |
+        -------------------------------------------------------------
+9       |     |     |     | 7X1 |     |     |     | 0X2 |     | 7X2 |
+        -------------------------------------------------------------
+10      |     | 2X1 |     |     |     |     |     |     | 5X1 |     |
+        -------------------------------------------------------------
+Soldado con mayor vida del ejercito 1: Soldado0X1 - 5
+Promedio de puntos de vida del ejercito 1: 3.333333
+Soldado0X1 - 5
+Soldado1X1 - 5
+Soldado2X1 - 2
+Soldado3X1 - 2
+Soldado4X1 - 2
+Soldado5X1 - 5
+Soldado6X1 - 4
+Soldado7X1 - 4
+Soldado8X1 - 1
+
+Soldado0X1 - 5
+Soldado1X1 - 5
+Soldado5X1 - 5
+Soldado6X1 - 4
+Soldado7X1 - 4
+Soldado2X1 - 2
+Soldado3X1 - 2
+Soldado4X1 - 2
+Soldado8X1 - 1
+
+Soldado0X1 - 5
+Soldado1X1 - 5
+Soldado5X1 - 5
+Soldado6X1 - 4
+Soldado7X1 - 4
+Soldado2X1 - 2
+Soldado3X1 - 2
+Soldado4X1 - 2
+Soldado8X1 - 1
+
+Soldado con mayor vida del ejercito 2: Soldado4X2 - 5
+Promedio de puntos de vida del ejercito 2: 2.777778
+Soldado0X2 - 2
+Soldado1X2 - 1
+Soldado2X2 - 3
+Soldado3X2 - 2
+Soldado4X2 - 5
+Soldado5X2 - 3
+Soldado6X2 - 5
+Soldado7X2 - 3
+Soldado8X2 - 1
+
+Soldado4X2 - 5
+Soldado6X2 - 5
+Soldado2X2 - 3
+Soldado5X2 - 3
+Soldado7X2 - 3
+Soldado0X2 - 2
+Soldado3X2 - 2
+Soldado1X2 - 1
+Soldado8X2 - 1
+
+Soldado4X2 - 5
+Soldado6X2 - 5
+Soldado2X2 - 3
+Soldado5X2 - 3
+Soldado7X2 - 3
+Soldado3X2 - 2
+Soldado0X2 - 2
+Soldado1X2 - 1
+Soldado8X2 - 1
+
+Gana el ejercito 1 con 30 a 25 puntos de vida!
