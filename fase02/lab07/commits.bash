86677b2b3ead97a5a124f0ade5b57eac77359973
Christian Mestas Zegarra <cmestasz@unsa.edu.pe>
2023-10-18 20:51:16 -0500

    Informe del laboratorio 07

---
+++ fase02/lab07/commits.bash
@@ -0,0 +1,64 @@
+# CREACION DE CARPETAS Y ARCHIVOS COMO PLANTILLA
+$ mkdir lab07
+$ cd lab07
+$ code Soldado.java
+$ code VideoJuego4.java
+$ git add fase02/lab07/Soldado.java
+$ git add fase02/lab07/VideoJuego4.java
+$ git commit -m "Plantilla en main"
+[main edcc77a] Plantilla en main
+ 2 files changed, 131 insertions(+)
+ create mode 100644 fase02/lab07/Soldado.java
+ create mode 100644 fase02/lab07/VideoJuego4.java
+# METODO QUE CONTIENE LA BATALLA, PARA PERMITIR EL COMPORTAMIENTO ITERATIVO
+$ git add fase02/lab07/VideoJuego4.java
+$ git commit -m "Metodo simularBatalla()"
+[main c13882a] Metodo simularBatalla()
+ 1 file changed, 11 insertions(+)
+# METODO QUE INICIALIZA LOS SOLDADOS
+$ git add fase02/lab07/VideoJuego4.java
+$ git commit -m "Metodo inicializarSoldados()"
+[main 01bb7b2] Metodo inicializarSoldados()
+ 1 file changed, 17 insertions(+), 1 deletion(-)
+# METODO QUE IMPRIME EL TABLERO CON EL NOMBRE Y EL EQUIPO DE CADA SOLDADO
+$ git add fase02/lab07/VideoJuego4.java
+$ git commit -m "Metodo imprimirTablero() y auxiliares"
+[main ad06cf5] Metodo imprimirTablero() y auxiliares
+ 1 file changed, 30 insertions(+)
+# METODO QUE RETORNA EL SOLDADO CON MAYOR VIDA DE UN EJERCITO
+$ git add fase02/lab07/VideoJuego4.java
+$ git commit -m "Metodo soldadoMayorVida()"
+[main d9efb19] Metodo soldadoMayorVida()
+ 1 file changed, 6 insertions(+)
+# METODO QUE RETORNA EL PROMEDIO DE PUNTOS DE VIDA DE UN EJERCITO
+$ git add fase02/lab07/VideoJuego4.java
+$ git commit -m "Metodo promedioPuntosVida()"
+[main a6511d1] Metodo promedioPuntosVida()
+ 1 file changed, 2 insertions(+)
+# METODO QUE IMPRIME LA LISTA DE SOLDADOS DE UN EJERCITO
+$ git add fase02/lab07/VideoJuego4.java
+$ git commit -m "Metodo imprimirSoldados()"
+[main 22054ad] Metodo imprimirSoldados()
+ 1 file changed, 2 insertions(+)
+# METODO QUE COPIA LA LISTA DE SOLDADOS DE UN EJERCITO
+$ git add fase02/lab07/VideoJuego4.java
+$ git commit -m "Metodo copiarSoldados()"
+[main 6d0fe11] Metodo copiarSoldados()
+ 1 file changed, 2 insertions(+)
+# METODO QUE ORDENA LOS SOLDADOS DE UN EJERCITO USANDO EL ORDENAMIENTO BURBUJA
+$ git add fase02/lab07/VideoJuego4.java
+$ git commit -m "Metodo ordenarSoldadosBurbuja() y auxiliar"
+[main 6f49c2e] Metodo ordenarSoldadosBurbuja()
+ 1 file changed, 8 insertions(+)
+# METODO QUE ORDENA LOS SOLDADOS DE UN EJERCITO USANDO EL ORDENAMIENTO SELECCION
+$ git add fase02/lab07/VideoJuego4.java
+$ git commit -m "Metodo ordenarSoldadosSeleccion()"
+[main dfdff96] Metodo ordenarSoldadosSeleccion()
+ 1 file changed, 10 insertions(+)
+# METODO QUE IMPRIME EL EJERCITO GANADOR DE LA BATALLA
+$ git add fase02/lab07/VideoJuego4.java
+$ git commit -m "Metodo imprimirGanador() y auxiliar"
+[main 4e3a23c] Metodo imprimirGanador() y auxiliar
+ 1 file changed, 12 insertions(+)
+# PUSH FINAL
+$ git push
---
+++ fase02/lab07/ejec01.bash
@@ -0,0 +1,150 @@
+           A     B     C     D     E     F     G     H     I     J   
+        -------------------------------------------------------------
+1       |     |     |     |     |     |     |     |     |     |     |
+        -------------------------------------------------------------
+2       |     | 8X1 |     |     |     |     |     |     |     |     |
+        -------------------------------------------------------------
+3       | 3X1 | 6X1 |     |     |     |     |     |     |     |     |
+        -------------------------------------------------------------
+4       | 1X1 |     |     |     |     |     |     |     | 7X1 |     |
+        -------------------------------------------------------------
+5       |     |     |     |     |     |     |     | 2X1 |     |     |
+        -------------------------------------------------------------
+6       |     |     |     |     |     |     |     |     | 5X1 |     |
+        -------------------------------------------------------------
+7       | 4X1 |     |     |     |     |     |     |     |     |     |
+        -------------------------------------------------------------
+8       |     |     |     |     |     |     |     |     |     |     |
+        -------------------------------------------------------------
+9       | 0X1 |     |     |     | 0X2 |     |     |     |     |     |
+        -------------------------------------------------------------
+10      |     |     |     |     |     |     |     |     |     |     |
+        -------------------------------------------------------------
+Soldado con mayor vida del ejercito 1: Soldado1X1 - 5
+Promedio de puntos de vida del ejercito 1: 2.888889
+
+Soldados por orden de creacion:
+Soldado0X1 - 3
+Soldado1X1 - 5
+Soldado2X1 - 2
+Soldado3X1 - 2
+Soldado4X1 - 3
+Soldado5X1 - 5
+Soldado6X1 - 2
+Soldado7X1 - 3
+Soldado8X1 - 1
+
+Soldados ordenados por burbuja
+Soldado1X1 - 5
+Soldado5X1 - 5
+Soldado0X1 - 3
+Soldado4X1 - 3
+Soldado7X1 - 3
+Soldado2X1 - 2
+Soldado3X1 - 2
+Soldado6X1 - 2
+Soldado8X1 - 1
+
+Soldados ordenados por seleccion
+Soldado1X1 - 5
+Soldado5X1 - 5
+Soldado4X1 - 3
+Soldado0X1 - 3
+Soldado7X1 - 3
+Soldado3X1 - 2
+Soldado6X1 - 2
+Soldado2X1 - 2
+Soldado8X1 - 1
+
+Soldado con mayor vida del ejercito 2: Soldado0X2 - 3
+Promedio de puntos de vida del ejercito 2: 3.000000
+
+Soldados por orden de creacion:
+Soldado0X2 - 3
+
+Soldados ordenados por burbuja
+Soldado0X2 - 3
+
+Soldados ordenados por seleccion
+Soldado0X2 - 3
+
+Gana el ejercito 1 con 26 a 3 puntos de vida!
+Desea simular otra batalla? (S/N): S
+           A     B     C     D     E     F     G     H     I     J   
+        -------------------------------------------------------------
+1       |     |     |     | 0X1 |     |     |     |     |     | 1X2 |
+        -------------------------------------------------------------
+2       |     |     |     |     |     |     |     |     |     |     |
+        -------------------------------------------------------------
+3       |     |     |     |     |     |     |     |     |     |     |
+        -------------------------------------------------------------
+4       |     |     |     |     |     |     | 3X2 |     |     |     |
+        -------------------------------------------------------------
+5       |     |     | 2X2 |     |     |     |     |     |     | 1X1 |
+        -------------------------------------------------------------
+6       |     |     |     |     | 5X2 |     | 4X2 |     |     |     |
+        -------------------------------------------------------------
+7       |     |     |     | 2X1 |     | 3X1 |     |     |     |     |
+        -------------------------------------------------------------
+8       |     | 0X2 |     |     |     |     | 4X1 |     |     |     |
+        -------------------------------------------------------------
+9       |     |     |     |     |     |     |     |     |     |     |
+        -------------------------------------------------------------
+10      |     |     |     |     |     | 6X2 |     |     |     |     |
+        -------------------------------------------------------------
+Soldado con mayor vida del ejercito 1: Soldado0X1 - 5
+Promedio de puntos de vida del ejercito 1: 3.800000
+
+Soldados por orden de creacion:
+Soldado0X1 - 5
+Soldado1X1 - 5
+Soldado2X1 - 3
+Soldado3X1 - 3
+Soldado4X1 - 3
+
+Soldados ordenados por burbuja
+Soldado0X1 - 5
+Soldado1X1 - 5
+Soldado2X1 - 3
+Soldado3X1 - 3
+Soldado4X1 - 3
+
+Soldados ordenados por seleccion
+Soldado0X1 - 5
+Soldado1X1 - 5
+Soldado2X1 - 3
+Soldado3X1 - 3
+Soldado4X1 - 3
+
+Soldado con mayor vida del ejercito 2: Soldado4X2 - 5
+Promedio de puntos de vida del ejercito 2: 3.714286
+
+Soldados por orden de creacion:
+Soldado0X2 - 4
+Soldado1X2 - 4
+Soldado2X2 - 3
+Soldado3X2 - 4
+Soldado4X2 - 5
+Soldado5X2 - 2
+Soldado6X2 - 4
+
+Soldados ordenados por burbuja
+Soldado4X2 - 5
+Soldado0X2 - 4
+Soldado1X2 - 4
+Soldado3X2 - 4
+Soldado6X2 - 4
+Soldado2X2 - 3
+Soldado5X2 - 2
+
+Soldados ordenados por seleccion
+Soldado4X2 - 5
+Soldado1X2 - 4
+Soldado3X2 - 4
+Soldado0X2 - 4
+Soldado6X2 - 4
+Soldado2X2 - 3
+Soldado5X2 - 2
+
+Gana el ejercito 2 con 26 a 19 puntos de vida!
+Desea simular otra batalla? (S/N): N
