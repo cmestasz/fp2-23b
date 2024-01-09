bdd4a618516b97d9aba22a10956f96843fda4ddd
Christian Mestas Zegarra <cmestasz@unsa.edu.pe>
2024-01-08 23:30:31 -0500

    Clase Lancero

--- fase03/lab20/Lancero.java
+++ fase03/lab20/Lancero.java
@@ -1,3 +1,12 @@
 public class Lancero extends Soldado {
+    private int longitudLanza;
+
+    public Lancero(String nombre, int equipo, int vidaInicial, int ataque, int defensa, String clase) {
+        super(nombre, equipo, vidaInicial, ataque, defensa, clase);
+    }
+
+    public void schiltrom() {
+        defender();
+    }
     
 }
