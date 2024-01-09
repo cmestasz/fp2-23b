da970e0d4c66569b0a6a90aef0218883364eca7c
Christian Mestas Zegarra <cmestasz@unsa.edu.pe>
2024-01-09 00:26:31 -0500

    Juego principal con comportamiento iterativo

--- fase03/lab20/Videojuego.java
+++ fase03/lab20/Videojuego.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
+
 public class Videojuego {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String respuesta = "S";
+        while (respuesta.equalsIgnoreCase("S")) {
+            new Mapa();
+            System.out.print("Desea jugar de nuevo? (S/N): ");
+            respuesta = sc.nextLine();
+        }
+    }
 }
