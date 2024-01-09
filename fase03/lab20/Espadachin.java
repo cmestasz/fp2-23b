07daea2262662bb3700129302367186202ffbb28
Christian Mestas Zegarra <cmestasz@unsa.edu.pe>
2024-01-08 23:24:25 -0500

    Clase Espadachin

--- fase03/lab20/Espadachin.java
+++ fase03/lab20/Espadachin.java
@@ -1,3 +1,12 @@
 public class Espadachin extends Soldado {
+    private double longitudEspada;
+
+    public Espadachin(String nombre, int equipo, int vidaInicial, int ataque, int defensa, String clase) {
+        super(nombre, equipo, vidaInicial, ataque, defensa, clase);
+    }
+
+    public void generarMuroEscudos() {
+        System.out.println(getNombre() + " genera un muro de escudos!");
+    }
     
 }
