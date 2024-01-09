333fdee4b9c18acf4f0dd2e2a197f7102fd1d529
Christian Mestas Zegarra <cmestasz@unsa.edu.pe>
2024-01-08 23:28:14 -0500

    Clase Arquero

--- fase03/lab20/Arquero.java
+++ fase03/lab20/Arquero.java
@@ -1,3 +1,15 @@
 public class Arquero extends Soldado {
-    
+    private int flechas = 10;
+
+    public Arquero(String nombre, int equipo, int vidaInicial, int ataque, int defensa, String clase) {
+        super(nombre, equipo, vidaInicial, ataque, defensa, clase);
+    }
+
+    public void disparar(Soldado otro) {
+        if (flechas > 0) {
+            atacar(otro, 1);
+            flechas--;
+        }
+    }
+
 }
