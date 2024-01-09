257ef022dd5f1a0af7bffafecc24f3d983739837
Christian Mestas Zegarra <cmestasz@unsa.edu.pe>
2024-01-08 22:31:54 -0500

    Clase Soldado base

--- fase03/lab20/Soldado.java
+++ fase03/lab20/Soldado.java
@@ -1,3 +1,19 @@
 public abstract class Soldado {
-    
+    private String nombre;
+    private String reino;
+    private int vidaInicial;
+    private int vidaActual;
+    private int ataque;
+    private int defensa;
+    private String clase;
+
+    public Soldado(String nombre, String reino, int vidaInicial, int ataque, int defensa, String clase) {
+        this.nombre = nombre;
+        this.reino = reino;
+        this.vidaInicial = vidaInicial;
+        this.vidaActual = vidaInicial;
+        this.ataque = ataque;
+        this.defensa = defensa;
+        this.clase = clase;
+    }
 }
