6377d8b0b9b4c670c9fd06065a11d5903cf940eb
Christian Mestas Zegarra <cmestasz@unsa.edu.pe>
2024-01-14 00:49:26 -0500

    Clase Videojuego para el comportamiento iterativo

--- fase03/lab22/Videojuego.java
+++ fase03/lab22/Videojuego.java
@@ -1,15 +1,19 @@
 package fase03.lab22;
 
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Videojuego {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String respuesta = "S";
-        while (respuesta.equalsIgnoreCase("S")) {
-            new Mapa();
-            System.out.print("Desea jugar de nuevo? (S/N): ");
-            respuesta = sc.nextLine();
-        }
+        new Videojuego();
+    }
+
+    public Videojuego() {
+        new MapaSuperior(this);
+    }
+
+    public void continuar() {
+        int continuar = JOptionPane.showConfirmDialog(null, "Desea jugar de nuevo? (S/N): ");
+        if (continuar == JOptionPane.YES_OPTION)
+            new MapaSuperior(this);
     }
 }
